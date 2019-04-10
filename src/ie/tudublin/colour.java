996aacb9a640f967d544386d5d8c291f8076dff5
Stephen <c17455002@mydit.ie>
2019-04-10 17:14:39 +0100

    code to get colour

--- src/ie/tudublin/colour.java
+++ src/ie/tudublin/colour.java
@@ -1,8 +1,20 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
+import processing.data.Table;
+import processing.data.TableRow;
+
+//imports don't appear to work
 
 public class colour
 {
-    
+    public void addColour(String string, String string2) {
+        Table table = addColour("colours.csv", "header");
+
+
+        for (TableRow row : table.rows()) {
+            Object colour = new colour(row);
+            ((Object) colour).add(colour);
+        }
+    }
 }
